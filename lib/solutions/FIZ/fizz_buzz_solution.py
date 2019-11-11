78b16374d3105c5b043aaf89c763887799ade7c6
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 14:44:08 +0000

    Mon Nov 11 14:44:08 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,7 +1,7 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
     if 1 <= number <= 9999:
-        if number > 10 and len(set(str(number))) == 1:
+        if number > 10 and len(set(str(number))) == 1 and number % 2 != 0:
             if number % 3 == 0 or '3' in str(number):
                 return 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
             elif number % 5 == 0 or '5' in str(number):
@@ -16,6 +16,7 @@ def fizz_buzz(number):
             else:
                 return number
     # raise NotImplementedError()
+
 
 
 
