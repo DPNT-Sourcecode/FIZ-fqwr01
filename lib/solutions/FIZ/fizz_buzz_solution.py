be6ef2ac964655e1abc77b7c88e278d8263f4e34
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 15:05:11 +0000

    Mon Nov 11 15:05:11 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -17,3 +17,25 @@ def fizz_buzz(number):
             else:
                 output = number
     return output.replace('deluxe','fake deluxe') if number % 2 != 0 and isinstance(output,str) else output
+
+# noinspection PyUnusedLocal
+def fizz_buzz(number):
+    if 1 <= number <= 9999:
+        output = ''
+        if (number % 3 == 0 and '3' in str(number)) or (number % 5 == 0 and '5' in str(number)):
+            if number % 3 == 0 or '3' in str(number):
+                output = 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
+            elif number % 5 == 0 or '5' in str(number):
+                output = 'fizz buzz deluxe' if number % 3 == 0 or '3' in str(number) else 'buzz deluxe'
+            else:
+                output = 'deluxe'
+        else:
+            if number % 3 == 0 or '3' in str(number):
+                output = 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
+            elif number % 5 == 0 or '5' in str(number):
+                output = 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
+            else:
+                output = number
+    return output.replace('deluxe','fake deluxe') if number % 2 != 0 and isinstance(output,str) else output
+
+
