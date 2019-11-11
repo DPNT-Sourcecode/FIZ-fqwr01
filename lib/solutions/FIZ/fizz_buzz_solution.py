537703611776835fa0213811ccd1404fc1ae5b81
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 14:32:41 +0000

    Mon Nov 11 14:32:41 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,14 +1,11 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
     if 1 <= number <= 9999:
-        if (number % 3 == 0 and number % 5 == 0) or ('3' in str(number) and '5' in str(number)):
-            return 'fizz buzz'
-        elif number % 3 == 0 or '3' in str(number):
-            return 'fizz'
-        elif number % 5 == 0 or '5' in str(number):
-            return 'buzz'
-        else:
-            return number
+        if number % 3 == 0 or '3' in str(number):
+            return 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
+        if number % 5 == 0 or '5' in str(number):
+            return 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
     # raise NotImplementedError()
+
 
 
