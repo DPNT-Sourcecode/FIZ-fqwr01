20fc3e12145fc6c06e89526ddb616fa622caa99d
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 14:57:06 +0000

    Mon Nov 11 14:57:06 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,22 +3,20 @@ def fizz_buzz(number):
     if 1 <= number <= 9999:
         output = ''
         if number > 10 and len(set(str(number))) == 1:
-            # if number % 2 != 0:
-            #     fake_deluxe = 'fake deluxe'
             if number % 3 == 0 or '3' in str(number):
-                output = 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else output = 'fizz deluxe'
+                output = 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
             elif number % 5 == 0 or '5' in str(number):
-                output = 'fizz buzz deluxe' if number % 3 == 0 or '3' in str(number) else output = 'buzz deluxe'
+                output = 'fizz buzz deluxe' if number % 3 == 0 or '3' in str(number) else 'buzz deluxe'
             else:
                 return 'deluxe'
         else:
             if number % 3 == 0 or '3' in str(number):
-                output = 'fizz buzz' if number % 5 == 0 or '5' in str(number) else output = 'fizz'
+                output = 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
             elif number % 5 == 0 or '5' in str(number):
-                output = 'fizz buzz' if number % 3 == 0 or '3' in str(number) else output = 'buzz'
+                output = 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
             else:
                 output = number
-    return output.repl
-    # raise NotImplementedError()
+    return output.replace('deluxe','fake deluxe') if number % 2 != 0 else output
+
 
 
