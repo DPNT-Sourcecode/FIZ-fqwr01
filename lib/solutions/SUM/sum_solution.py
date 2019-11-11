db7a8dd1152060525ffa420797c37c609a9989c6
sbx_user1051 <sbx_user1051@169.254.141.37>
2019-11-11 13:51:36 +0000

    Mon Nov 11 13:51:36 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    if 0 <= x <= 100 and 0 <= x <= 100:
+        return 
+
