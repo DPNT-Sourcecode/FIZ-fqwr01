13b9fdef2acbc8796aacb6058c1c4d2abef30668
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 15:08:11 +0000

    Mon Nov 11 15:08:11 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,26 +2,6 @@
 def fizz_buzz(number):
     if 1 <= number <= 9999:
         output = ''
-        if number > 10 and len(set(str(number))) == 1:
-            if number % 3 == 0 or '3' in str(number):
-                output = 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
-            elif number % 5 == 0 or '5' in str(number):
-                output = 'fizz buzz deluxe' if number % 3 == 0 or '3' in str(number) else 'buzz deluxe'
-            else:
-                output = 'deluxe'
-        else:
-            if number % 3 == 0 or '3' in str(number):
-                output = 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
-            elif number % 5 == 0 or '5' in str(number):
-                output = 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
-            else:
-                output = number
-    return output.replace('deluxe','fake deluxe') if number % 2 != 0 and isinstance(output,str) else output
-
-# noinspection PyUnusedLocal
-def fizz_buzz(number):
-    if 1 <= number <= 9999:
-        output = ''
         if (number % 3 == 0 and '3' in str(number)) or (number % 5 == 0 and '5' in str(number)):
             if number % 3 == 0 or '3' in str(number):
                 output = 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
@@ -37,5 +17,6 @@ def fizz_buzz(number):
             else:
                 output = number
     return output.replace('deluxe','fake deluxe') if number % 2 != 0 and isinstance(output,str) else output
+
 
 
