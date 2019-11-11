6c8853041984e96c9fc1270e8883bd480129983b
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 14:49:37 +0000

    Mon Nov 11 14:49:37 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,7 +3,7 @@ def fizz_buzz(number):
     if 1 <= number <= 9999:
         if number > 10 and len(set(str(number))) == 1:
             if number % 2 != 0:
-                return 'fake deluxe'
+                fake_deluxe = 'fake deluxe'
             if number % 3 == 0 or '3' in str(number):
                 return 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
             elif number % 5 == 0 or '5' in str(number):
@@ -18,9 +18,3 @@ def fizz_buzz(number):
             else:
                 return number
     # raise NotImplementedError()
-
-
-
-
-
-
