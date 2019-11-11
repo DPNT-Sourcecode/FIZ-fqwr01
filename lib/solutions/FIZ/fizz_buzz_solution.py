99351d01ca8ff0cd8e8d320f5f6301126d62687a
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 14:33:14 +0000

    Mon Nov 11 14:33:14 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -5,7 +5,10 @@ def fizz_buzz(number):
             return 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
         if number % 5 == 0 or '5' in str(number):
             return 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
+        else:
+            number
     # raise NotImplementedError()
+
 
 
 
