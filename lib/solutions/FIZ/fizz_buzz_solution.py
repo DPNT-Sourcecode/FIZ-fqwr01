a6889681770432e69eec61b2ec35109771f181e9
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 14:34:00 +0000

    Mon Nov 11 14:34:00 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,12 +3,8 @@ def fizz_buzz(number):
     if 1 <= number <= 9999:
         if number % 3 == 0 or '3' in str(number):
             return 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
-        if number % 5 == 0 or '5' in str(number):
+        elif number % 5 == 0 or '5' in str(number):
             return 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
         else:
             number
     # raise NotImplementedError()
-
-
-
-
