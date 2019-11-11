26556382bbab1ed58007851712571d93b48ee224
sbx_user1051 <sbx_user1051@169.254.47.133>
2019-11-11 15:09:36 +0000

    Mon Nov 11 15:09:36 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,21 +2,24 @@
 def fizz_buzz(number):
     if 1 <= number <= 9999:
         output = ''
-        if (number % 3 == 0 and '3' in str(number)) or (number % 5 == 0 and '5' in str(number)):
-            if number % 3 == 0 or '3' in str(number):
-                output = 'fizz buzz deluxe' if number % 5 == 0 or '5' in str(number) else 'fizz deluxe'
-            elif number % 5 == 0 or '5' in str(number):
-                output = 'fizz buzz deluxe' if number % 3 == 0 or '3' in str(number) else 'buzz deluxe'
+        three_multiple = number % 3 == 0
+        five_multipe = number % 5 == 0
+        if (three_multiple and '3' in str(number)) or (five_multipe and '5' in str(number)):
+            if three_multiple or '3' in str(number):
+                output = 'fizz buzz deluxe' if five_multipe or '5' in str(number) else 'fizz deluxe'
+            elif five_multipe or '5' in str(number):
+                output = 'fizz buzz deluxe' if three_multiple or '3' in str(number) else 'buzz deluxe'
             else:
                 output = 'deluxe'
         else:
-            if number % 3 == 0 or '3' in str(number):
-                output = 'fizz buzz' if number % 5 == 0 or '5' in str(number) else 'fizz'
-            elif number % 5 == 0 or '5' in str(number):
-                output = 'fizz buzz' if number % 3 == 0 or '3' in str(number) else 'buzz'
+            if three_multiple or '3' in str(number):
+                output = 'fizz buzz' if five_multipe or '5' in str(number) else 'fizz'
+            elif five_multipe or '5' in str(number):
+                output = 'fizz buzz' if three_multiple or '3' in str(number) else 'buzz'
             else:
                 output = number
     return output.replace('deluxe','fake deluxe') if number % 2 != 0 and isinstance(output,str) else output
+
 
 
 
